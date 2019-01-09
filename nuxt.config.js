ff816ebfc47853f04e156501bdd3b052f6a6bab6
Louis Bluhm <suspiciouspotato@gmail.com>
2019-01-09 16:46:23 +0000

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'JSON',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        href: 'https://fonts.googleapis.com/css?family=Roboto+Mono',
+        rel: 'stylesheet'
+      }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: ['element-ui/lib/theme-chalk/index.css'],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: ['@/plugins/element-ui'],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <div id="editor"/>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      darkTheme: true
+    }
+  },
+  async mounted() {
+    if (process.browser) {
+      const ace = await import('brace')
+
+      /* Plugins */
+      require('brace/mode/json')
+      require('brace/theme/dracula')
+      require('brace/theme/github')
+
+      const editor = ace.edit('editor')
+      editor.getSession().setMode('ace/mode/json')
+      editor.setTheme('ace/theme/dracula')
+
+      editor.setShowPrintMargin(false)
+
+      editor.setOptions({
+        fontFamily: 'Roboto Mono',
+        fontSize: 15,
+        wrap: true,
+        indentedSoftWrap: false
+      })
+
+      editor.commands.addCommand({
+        name: 'format',
+        bindKey: { win: 'Ctrl+Enter', mac: 'Ctrl+Enter' },
+        exec: editor => {
+          this.format(editor)
+        }
+      })
+
+      editor.commands.addCommand({
+        name: 'toggleTheme',
+        bindKey: { win: 'Ctrl+b', mac: 'Ctrl+b' },
+        exec: editor => {
+          this.toggleTheme(editor)
+        }
+      })
+
+      // TODO: Just for debugging
+      editor.setValue(`{
+  "How to use": "Paste your JSON here and press Ctrl+Enter to format!",
+  "Help": "Check the console for errors if it fails to parse.",
+  "Themes": "Toggle dark/light theme with Ctrl+B"
+}\n`)
+
+      editor.clearSelection()
+      editor.focus()
+    }
+  },
+  methods: {
+    format(editor) {
+      const text = editor.getValue()
+
+      if (!text) {
+        return
+      }
+
+      try {
+        const formattedText = JSON.stringify(JSON.parse(text), null, 2)
+        editor.setValue(formattedText)
+        editor.gotoLine(1)
+        editor.scrollToLine(1, 1, true)
+        // editor.setReadOnly(true)
+      } catch (err) {
+        console.error('Error formatting JSON')
+        console.error(err)
+      }
+    },
+    toggleTheme(editor) {
+      if (this.darkTheme) {
+        editor.setTheme('ace/theme/github')
+        this.darkTheme = false
+      } else {
+        editor.setTheme('ace/theme/dracula')
+        this.darkTheme = true
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#editor {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+</style>
