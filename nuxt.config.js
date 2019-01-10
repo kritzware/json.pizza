4356dc5410748280165112eb495cda9942507deb
Louis Bluhm <suspiciouspotato@gmail.com>
2019-01-10 22:30:46 +0000

    fix: meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'JSON prettifier, formatter and parser'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
