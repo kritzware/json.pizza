30c777b0fd357d20d0d74d2a61dc0059fba34725
Louis Bluhm <suspiciouspotato@gmail.com>
2019-01-10 14:20:23 +0000

    New icon image

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'JSON',
+    title: 'json.pizza',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
