2b3f3e2d4e705766c66056577325c4f21e8e3422
Louis Bluhm <suspiciouspotato@gmail.com>
2019-08-15 23:31:38 +0100

    finished todo items for digi-com pr

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <div class="container">
     <div class="sidebar">
       <div class="icon">🍕</div>
-      <svg :class="{ 'active': info }" class="info-icon" width="25" height="25" xmlns="http://www.w3.org/2000/svg" @click="info = !info"><path d="M12.5 0C19.3927083 0 25 5.607292 25 12.5S19.3927083 25 12.5 25 0 19.392708 0 12.5 5.6072917 0 12.5 0zm0 2C6.710125 2 2 6.710125 2 12.5S6.710125 23 12.5 23 23 18.289875 23 12.5 18.289875 2 12.5 2zm2.158 16.284c-.661.26-2.952 1.354-4.272.191-.394-.346-.59-.785-.59-1.318 0-.998.328-1.868.919-3.957.104-.395.231-.907.231-1.313 0-.701-.266-.887-.987-.887-.352 0-.742.125-1.095.257l.195-.799c.787-.32 1.775-.71 2.621-.71 1.269 0 2.203.633 2.203 1.837 0 .347-.06.955-.186 1.375l-.73 2.582c-.151.522-.424 1.673-.001 2.014.416.337 1.401.158 1.887-.071l-.195.799zM13.452 8c-.828 0-1.5-.672-1.5-1.5s.672-1.5 1.5-1.5 1.5.672 1.5 1.5-.672 1.5-1.5 1.5z" fill="#FFF" fill-rule="nonzero"/></svg>
+      <svg :class="{ 'active': info }" class="info-icon" width="25" height="25" xmlns="http://www.w3.org/2000/svg" @click="toggleInfoBox"><path d="M12.5 0C19.3927083 0 25 5.607292 25 12.5S19.3927083 25 12.5 25 0 19.392708 0 12.5 5.6072917 0 12.5 0zm0 2C6.710125 2 2 6.710125 2 12.5S6.710125 23 12.5 23 23 18.289875 23 12.5 18.289875 2 12.5 2zm2.158 16.284c-.661.26-2.952 1.354-4.272.191-.394-.346-.59-.785-.59-1.318 0-.998.328-1.868.919-3.957.104-.395.231-.907.231-1.313 0-.701-.266-.887-.987-.887-.352 0-.742.125-1.095.257l.195-.799c.787-.32 1.775-.71 2.621-.71 1.269 0 2.203.633 2.203 1.837 0 .347-.06.955-.186 1.375l-.73 2.582c-.151.522-.424 1.673-.001 2.014.416.337 1.401.158 1.887-.071l-.195.799zM13.452 8c-.828 0-1.5-.672-1.5-1.5s.672-1.5 1.5-1.5 1.5.672 1.5 1.5-.672 1.5-1.5 1.5z" fill="#FFF" fill-rule="nonzero"/></svg>
       <transition name="fade">
-        <div v-if="info" class="info-box">
+        <div v-click-outside="hideInfoBox" v-if="info" class="info-box">
           <b>How to use:</b> Paste your JSON in the editor and press <b>Format</b> (Ctrl+Enter).<br>
-          <b>Share:</b> To copy a shareable URL to your clipboard, click <b>Share</b> (Ctrl+L).<br>
+          <!-- <b>Share:</b> To copy a shareable URL to your clipboard, click <b>Share</b> (Ctrl+L).<br> -->
           <b>Errors:</b> Check the console for errors if your JSON fails to parse.<br>
           <b>Themes:</b> Toggle between dark/light themes by pressing Ctrl+B.<br>
           <b>Privacy:</b> Your data will not be stored or shared with any third parties.<br>
@@ -18,26 +18,47 @@
       <div id="editor"/>
     </div>
     <div class="action-buttons">
-      <button>
+      <button @click="format(editor)">
         <svg style="margin-right: 10px;" width="16" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h16v2H0V0zm0 12h16v2H0v-2zm0-4h6v2H0V8zm0-4h6v2H0V4zm12 2V3l4 4-4 4V8H8V6h4z" fill="#FFF" fill-rule="nonzero"/></svg>
         Format
       </button>
-      <button>
+      <button @click="copy">
         <svg style="margin-right: 10px;" width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M15 5c.6 0 1 .4 1 1v9c0 .6-.4 1-1 1H8c-.6 0-1-.4-1-1V6c0-.6.4-1 1-1h7zM5 4v10H1c-.6 0-1-.4-1-1V1c0-.6.4-1 1-1h10c.6 0 1 .4 1 1v2H6c-.6 0-1 .4-1 1z" fill="#FFF" fill-rule="nonzero"/></svg>
         Copy
       </button>
-      <button>
+      <!-- <button>
         <svg style="margin-right: 10px;" width="16" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M8 4C2.8 4 0 7.8 0 14c1.5-2.4 2.7-4 8-4v4l8-7-8-7v4z" fill="#FFF" fill-rule="nonzero"/></svg>
         Share
+      </button> -->
+      <button @click="toggleTheme(editor)">
+        <div style="font-size:25px;vertical-align:middle;line-height:1">
+          <div v-if="darkTheme">🌞</div>
+          <div v-else>🌚</div>
+        </div>
       </button>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
+Vue.directive('click-outside', {
+  bind: function(el, binding, vnode) {
+    el.clickOutsideEvent = function(event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event)
+      }
+    }
+    document.body.addEventListener('click', el.clickOutsideEvent)
+  },
+  unbind: function(el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+  }
+})
+
 const DEFAULT_TEXT = `{
   "How to use": "Paste your JSON in the editor and press Format (Ctrl+Enter).",
-  "Share": "To copy a shareable URL to your clipboard, click Share (Ctrl+L).",
   "Errors": "Check the console for errors if your JSON fails to parse.",
   "Themes": "Toggle between dark/light theme by pressing Ctrl+B",
   "Privacy": "Your data will not be stored or shared with any third parties.",
@@ -48,11 +69,17 @@ export default {
   data() {
     return {
       darkTheme: true,
-      info: false
+      info: false,
+      editor: null
     }
   },
   async mounted() {
     if (process.browser) {
+      const seenInfo = localStorage.getItem('jsonpizza:seen-info')
+      if (!seenInfo) {
+        this.info = true
+      }
+
       const ace = await import('brace')
 
       // Plugins
@@ -63,13 +90,13 @@ export default {
 
       // Editor config
 
-      const editor = ace.edit('editor')
-      editor.getSession().setMode('ace/mode/json')
-      editor.setTheme('ace/theme/vibrant_ink')
+      this.editor = ace.edit('editor')
+      this.editor.getSession().setMode('ace/mode/json')
+      this.editor.setTheme('ace/theme/vibrant_ink')
 
-      editor.setShowPrintMargin(false)
+      this.editor.setShowPrintMargin(false)
 
-      editor.setOptions({
+      this.editor.setOptions({
         fontFamily: 'Roboto Mono',
         fontSize: 14,
         wrap: true,
@@ -78,7 +105,7 @@ export default {
         indentedSoftWrap: false
       })
 
-      editor.commands.addCommand({
+      this.editor.commands.addCommand({
         name: 'format',
         bindKey: { win: 'Ctrl+Enter', mac: 'Ctrl+Enter' },
         exec: editor => {
@@ -86,7 +113,7 @@ export default {
         }
       })
 
-      editor.commands.addCommand({
+      this.editor.commands.addCommand({
         name: 'toggleTheme',
         bindKey: { win: 'Ctrl+b', mac: 'Ctrl+b' },
         exec: editor => {
@@ -94,7 +121,7 @@ export default {
         }
       })
 
-      editor.commands.addCommand({
+      this.editor.commands.addCommand({
         name: 'getShareableLink',
         bindKey: { win: 'Ctrl+l', mac: 'Ctrl+l' },
         exec: editor => {
@@ -102,7 +129,7 @@ export default {
         }
       })
 
-      editor.commands.addCommand({
+      this.editor.commands.addCommand({
         name: 'showHelp',
         bindKey: { win: 'Ctrl+i', mac: 'Ctrl+i' },
         exec: editor => {
@@ -110,8 +137,8 @@ export default {
         }
       })
 
-      editor.commands.bindKeys({ 'ctrl-l': null })
-      editor.commands.removeCommands(['gotoline', 'find'])
+      this.editor.commands.bindKeys({ 'ctrl-l': null })
+      this.editor.commands.removeCommands(['gotoline', 'find'])
 
       const { d } = this.$route.query
       let decoded = null
@@ -122,21 +149,20 @@ export default {
 
       const cached = localStorage.getItem('jsoncache')
       if (cached) {
-        editor.setValue(cached)
+        this.editor.setValue(cached)
       } else {
-        editor.setValue(decoded || DEFAULT_TEXT)
+        this.editor.setValue(decoded || DEFAULT_TEXT)
       }
 
-      editor.clearSelection()
-      if (decoded) {
-        editor.gotoLine(1)
-        editor.scrollToLine(1, 1, true)
-      }
-      editor.focus()
+      this.editor.clearSelection()
+      this.editor.gotoLine(1)
+      this.editor.scrollToLine(1, 1, true)
+      this.editor.focus()
     }
   },
   methods: {
     format(editor) {
+      this.hideInfoBox()
       const text = editor.getValue()
 
       if (!text) {
@@ -180,6 +206,25 @@ export default {
     },
     showHelp(editor) {
       alert(DEFAULT_TEXT)
+    },
+    copy() {
+      this.editor.selectAll()
+      this.editor.focus()
+      document.execCommand('copy')
+      this.editor.clearSelection()
+      this.editor.gotoLine(1)
+      this.editor.scrollToLine(1, 1, true)
+    },
+    hideInfoBox() {
+      if (!this.info) return
+      this.info = false
+      localStorage.setItem('jsonpizza:seen-info', 1)
+    },
+    toggleInfoBox() {
+      this.info = !this.info
+      if (!localStorage.getItem('jsonpizza:seen-info')) {
+        localStorage.setItem('jsonpizza:seen-info', 1)
+      }
     }
   }
 }
